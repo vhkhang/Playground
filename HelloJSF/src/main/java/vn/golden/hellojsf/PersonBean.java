a914b4ca0deccf82dfca9f9a1d37ad98dd2c648a
Khang Vu Hien <vhkhang@aavn.local>
2017-11-30 15:15:26 +0700

    create HelloJSF project

---
+++ HelloJSF/src/main/java/vn/golden/hellojsf/PersonBean.java
@@ -0,0 +1,31 @@
+package vn.golden.hellojsf;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class PersonBean {
+
+	private String name;
+	
+	private String link;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+	
+
+}
